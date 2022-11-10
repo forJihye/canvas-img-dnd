1df022739b0ea9e09f3184165f39a8e525ce469f
jihye <byjihye@hashsnap.net>
2022-11-10 08:29:07 +0000

    init commit

---
+++ src/main.ts
@@ -0,0 +1,115 @@
+import addDragControl from './drag-control';
+import './style.css'
+import addTouchControl from './touch-control';
+import { loadImage } from './utils';
+
+const app = document.getElementById('app') as HTMLDivElement;
+const assets = {
+  // photo1: "https://hashsnap-static.s3.ap-northeast-2.amazonaws.com/file/test/071a563e-8300-4dbf-b1a0-e0b2d0da530e.jpg",
+  photo3: "https://media.hashsn.app/uploaded-posts/de575236-7361-45a0-8065-1b2c2906dda8.jpg",
+  photo4: "https://media.hashsn.app/uploaded-posts/fb740d1c-4238-4d2d-9f8a-90094fd76ddc.jpg",
+  // photo2: "https://picsum.photos/1440/960",
+}
+
+const assetsMap: Map<string, (HTMLImageElement|null)> = new Map();
+const assetsLoad = Object.entries(assets).map(async (assets) => {
+  const [name, data] = assets;
+  const img = await loadImage(data) as HTMLImageElement;
+  assetsMap.set(name, img);
+  return [name, img];
+});
+await Promise.all(assetsLoad);
+
+const img = assetsMap.get('photo3') as HTMLImageElement;
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+canvas.width = 960;
+canvas.height = 960;
+
+const or = canvas.width / canvas.height // 투명영역 비율
+const ir = img.width / img.height // 이미지 비율
+const ratio = Math.min(canvas.width/img.width, canvas.height/img.height) // 종횡비 기준
+
+let mode = '';
+let iwidth = 0;
+let iheight = 0;
+let ix = 0;
+let iy = 0;
+
+let minX = 0;
+let minY = 0;
+let maxX = 0;
+let maxY = 0;
+
+if (or < ir) { // 높이 Fill
+  mode = 'vertical'
+  iwidth = (img.width * canvas.height) / img.height;
+  iheight = canvas.height;
+  ix = (canvas.width - iwidth) / 2;
+  iy = 0;
+  maxX = canvas.width - iwidth;
+  ctx.drawImage(img, ix, 0, iwidth, iheight);
+} 
+if (or > ir) { // 너비 Fill
+  mode = 'horizontal'
+  iwidth = canvas.width;
+  iheight = (img.height * canvas.width) / img.width;
+  ix = 0;
+  iy = (canvas.height - iheight) / 2;
+  maxY = canvas.height - iheight;
+  ctx.drawImage(img, 0, iy, canvas.width, iheight);
+} 
+
+addDragControl(canvas, {
+  down: (ev) => {
+    console.log('pointer down', ev)
+    return true;
+  },
+  move: (ev) => {
+    console.log('pointer move', ev)
+    if (mode === 'vertical') {
+      ix += ev.dx;
+      if (ix >= minX) ix = minX;
+      if (ix <= maxX) ix = maxX;
+      ctx.clearRect(0, 0, iwidth, iheight);
+      ctx.drawImage(img, ix, 0, iwidth, iheight);
+    } 
+    if (mode === 'horizontal') {
+      iy += ev.dy;
+      if (iy >= minY) iy = minY;
+      if (iy <= maxY) iy = maxY;
+      ctx.clearRect(0, 0, iwidth, iheight);
+      ctx.drawImage(img, 0, iy, iwidth, iheight);
+    }
+  },
+  up: (ev, payload) => {
+    console.log('pointer up', ev)
+  },
+})
+
+addTouchControl(canvas, {
+  down: (ev) => {
+    console.log('touch down', ev)
+    return true
+  },
+  move: (ev) => {
+    console.log('touch move', ev)
+    if (mode === 'vertical') {
+      ix += ev.dx;
+      if (ix >= minX) ix = minX;
+      if (ix <= maxX) ix = maxX;
+      ctx.clearRect(0, 0, iwidth, iheight);
+      ctx.drawImage(img, ix, 0, iwidth, iheight);
+    } 
+    if (mode === 'horizontal') {
+      iy += ev.dy;
+      if (iy >= minY) iy = minY;
+      if (iy <= maxY) iy = maxY;
+      ctx.clearRect(0, 0, iwidth, iheight);
+      ctx.drawImage(img, 0, iy, iwidth, iheight);
+    }
+  },
+  up: (ev) => {
+    console.log('touch up', ev)
+  }
+})
